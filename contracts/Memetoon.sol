05d735f9e4a0f42480190a36c4678c22dedda903
Pratap Sharma <sharma.pratap22@gmail.com>
2021-07-28 11:12:08 +0545

    Updated the smart contract

--- contracts/Memetoon.sol
+++ contracts/Memetoon.sol
@@ -5,6 +5,9 @@ import "@0xcert/ethereum-erc721/src/contracts/tokens/nf-token-metadata.sol";
 import "@0xcert/ethereum-erc721/src/contracts/ownership/ownable.sol";
 
 contract Memetoon is NFTokenMetadata, Ownable {
+    address payable public beneficiary = payable(address(0x3408F7D543569E6d803a7f2dB146AFca135D4002));
+    uint constant public minMintValue = 100000000000000;
+
     constructor() public {
         nftName = "Memetoon NFT";
         nftSymbol = "MTC";
@@ -14,8 +17,16 @@ contract Memetoon is NFTokenMetadata, Ownable {
         address _to,
         uint256 _tokenId,
         string calldata _uri
-    ) external {
+    ) public payable {
+
+        require(
+            msg.value <= minMintValue,
+            "Minting requires minimum."
+        );
+
         super._mint(_to, _tokenId);
         super._setTokenUri(_tokenId, _uri);
+
+        beneficiary.transfer(msg.value);
     }
 }
